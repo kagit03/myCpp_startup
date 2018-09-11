62727df5f72fe796700adb81aa54afa8dddbad75
Karthik <prasadkarthick450@gmail.com>
2018-09-11 07:06:19 +0200

    C ringbuffer function update

--- mangal_cpp_playground/ringbuffer.c
+++ mangal_cpp_playground/ringbuffer.c
@@ -9,13 +9,14 @@ int circ_buf_pus(int val){
 	int next;
 	next = head +1;
 	
-	if(next >= len){
+	if(next > len){
 		next=0;
 	}
 
-	if(next == tail){
+	/*if(next == tail){
+		head = next;
 		return -1;
-	}
+	}*/
 	circ_buf[head]=val;
 	head = next;
 	return 0;
@@ -24,29 +25,48 @@ int circ_buf_pus(int val){
 int circ_buf_pop(){
 	int next;
 	next = tail+1;
-	if (head == tail){
-		return -1;
-	}
-	if(next >= len){
+
+	if(next > len){
 		next=0;
 	}
+
+	if (next == head){
+		tail = next;
+		return -1;
+	}
+
 	tail = next;
 	return 	circ_buf[tail-1];
 }
 
 int main (){
-	int i=0, ret_val=0;
-	
-	for(i=0; i<=4; i++)
+	int i=0,j= 0,k=0, ret_val=0;
+	for(i=0; i<=5; i++)
 	{
-		ret_val = circ_buf_pus((i+1)*5);
+		ret_val = circ_buf_pus(i+5);
 		if (ret_val != 0){
 			ret_val = circ_buf_pop();
 			printf("%d popped out\n", ret_val);
+		}else{
+			printf("%d pushed in\n", circ_buf[i]);
 		}
-		printf("%d pushed in\n", circ_buf[i]);
 	}
-	//circ_buf_pop(20);
+
+	printf("\n\n");
+
+	for(k=0; k<=6; k++){
+		ret_val = circ_buf_pop();
+		if (ret_val < 0){
+			printf("Data not read even once\n");
+			//break;
+		}
+	}
+	
+	printf("\n\n");
+	for(j=0; j<len; j++){
+		printf("%d\n",circ_buf[j]);
+	}
+	printf("Head=%d Tail=%d\n",head, tail);
 
 	return 0;
 }
